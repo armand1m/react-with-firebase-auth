c3aa28b171f3e3f114643c1a780bbf10c1fb9434
Mike Francis <mikeffrancis@gmail.com>
2019-11-18 09:27:42 +0000

    Added loading state

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import * as firebase from 'firebase';
+import * as React from "react";
+import * as firebase from "firebase";
 
-import getErrorMessageForProvider from './getErrorMessageForProvider';
+import getErrorMessageForProvider from "./getErrorMessageForProvider";
 
 export type WrappedComponentProps = {
   signInWithEmailAndPassword: (email: string, password: string) => void;
@@ -10,25 +10,29 @@ export type WrappedComponentProps = {
   signInWithFacebook: () => void;
   signInWithGithub: () => void;
   signInWithTwitter: () => void;
-  signInWithPhoneNumber:  (phoneNumber: string, applicationVerifier: firebase.auth.ApplicationVerifier) => Promise<any>;
+  signInWithPhoneNumber: (
+    phoneNumber: string,
+    applicationVerifier: firebase.auth.ApplicationVerifier
+  ) => Promise<any>;
   signInAnonymously: () => void;
   signOut: () => void;
   setError: (error: any) => void;
   user?: firebase.User;
   error?: firebase.FirebaseError;
+  loading: boolean;
 };
 
 export type PossibleProviders =
-  'googleProvider'
-  | 'facebookProvider'
-  | 'twitterProvider'
-  | 'githubProvider';
+  | "googleProvider"
+  | "facebookProvider"
+  | "twitterProvider"
+  | "githubProvider";
 
 export type ProvidersMapper = {
   googleProvider?: firebase.auth.GithubAuthProvider_Instance;
   facebookProvider?: firebase.auth.FacebookAuthProvider_Instance;
   twitterProvider?: firebase.auth.TwitterAuthProvider_Instance;
-  githubProvider?:  firebase.auth.GithubAuthProvider_Instance;
+  githubProvider?: firebase.auth.GithubAuthProvider_Instance;
 };
 
 export type HocParameters = {
@@ -37,108 +41,131 @@ export type HocParameters = {
 };
 
 export type FirebaseAuthProviderState = {
-  user?: firebase.User,
-  error?: string,
+  loading: boolean;
+  user?: firebase.User;
+  error?: string;
 };
 
 const withFirebaseAuth = ({
   firebaseAppAuth,
-  providers = {},
-}: HocParameters) =>
-  (WrappedComponent: React.SFC<WrappedComponentProps>) =>
-    class FirebaseAuthProvider extends React.PureComponent<{}, FirebaseAuthProviderState> {
-      static displayName = `withFirebaseAuth(${WrappedComponent.displayName || WrappedComponent.name})`;
+  providers = {}
+}: HocParameters) => (WrappedComponent: React.SFC<WrappedComponentProps>) =>
+  class FirebaseAuthProvider extends React.PureComponent<
+    {},
+    FirebaseAuthProviderState
+  > {
+    static displayName = `withFirebaseAuth(${WrappedComponent.displayName ||
+      WrappedComponent.name})`;
 
-      state = {
-        user: undefined,
-        error: undefined,
-      };
+    state = {
+      loading: false,
+      user: undefined,
+      error: undefined
+    };
 
-      unsubscribeAuthStateListener: firebase.Unsubscribe;
+    unsubscribeAuthStateListener: firebase.Unsubscribe;
 
-      componentDidMount() {
-        this.unsubscribeAuthStateListener =
-          firebaseAppAuth.onAuthStateChanged((user: firebase.User) =>
-            this.setState({ user })
-          );
-      }
+    componentDidMount() {
+      this.setState({ loading: true });
 
-      componentWillUnmount() {
-        this.unsubscribeAuthStateListener();
-      }
+      this.unsubscribeAuthStateListener = firebaseAppAuth.onAuthStateChanged(
+        (user: firebase.User) => this.setState({ user, loading: false }),
+        (error: firebase.auth.Error) =>
+          this.setState({ error: error.message, loading: false }),
+        () => this.setState({ loading: false })
+      );
+    }
 
-      setError = (error: any) => this.setState({ error });
+    componentWillUnmount() {
+      this.unsubscribeAuthStateListener();
+    }
 
-      async tryTo <T> (operation: () => Promise<T>): Promise<T> {
-        try {
-          return operation();
-        } catch(error) {
-          this.setError(error.message);
-          return error;
-        }
-      };
+    setError = (error: any) => this.setState({ error });
 
-      tryToSignInWithProvider = (provider: PossibleProviders): Promise<firebase.auth.UserCredential> =>
-        this.tryTo<firebase.auth.UserCredential>(() => {
-          const providerInstance = providers[provider];
+    async tryTo<T>(operation: () => Promise<T>): Promise<T> {
+      try {
+        const callback = await operation();
 
-          if (!providerInstance) {
-            throw new Error(getErrorMessageForProvider(provider));
-          }
+        this.setState({ loading: false });
 
-          return firebaseAppAuth.signInWithPopup(providerInstance);
-        });
+        return callback;
+      } catch (error) {
+        this.setState({ error: error.message, loading: false });
 
-      signOut = () =>
-        this.tryTo<void>(() => firebaseAppAuth.signOut());
-
-      signInAnonymously = () =>
-        this.tryTo<firebase.auth.UserCredential>(() => firebaseAppAuth.signInAnonymously());
-
-      signInWithGithub = () =>
-        this.tryToSignInWithProvider('githubProvider');
-
-      signInWithTwitter = () =>
-        this.tryToSignInWithProvider('twitterProvider');
-
-      signInWithGoogle = () =>
-        this.tryToSignInWithProvider('googleProvider');
-
-      signInWithFacebook = () =>
-        this.tryToSignInWithProvider('facebookProvider');
-
-      signInWithEmailAndPassword = (email: string, password: string) =>
-        this.tryTo<firebase.auth.UserCredential>(() => firebaseAppAuth.signInWithEmailAndPassword(email, password));
-
-      signInWithPhoneNumber = (phoneNumber: string, applicationVerifier: firebase.auth.ApplicationVerifier) =>
-        this.tryTo<firebase.auth.ConfirmationResult>(() => firebaseAppAuth.signInWithPhoneNumber(phoneNumber, applicationVerifier))
-
-      createUserWithEmailAndPassword = (email: string, password: string) =>
-        this.tryTo<firebase.auth.UserCredential>(() => firebaseAppAuth.createUserWithEmailAndPassword(email, password));
-
-      sharedHandlers = {
-        signInWithEmailAndPassword: this.signInWithEmailAndPassword,
-        createUserWithEmailAndPassword: this.createUserWithEmailAndPassword,
-        signInWithGithub: this.signInWithGithub,
-        signInWithTwitter: this.signInWithTwitter,
-        signInWithGoogle: this.signInWithGoogle,
-        signInWithFacebook: this.signInWithFacebook,
-        signInWithPhoneNumber: this.signInWithPhoneNumber,
-        setError: this.setError,
-        signInAnonymously: this.signInAnonymously,
-        signOut: this.signOut,
-      };
-
-      render() {
-        const props = {
-          ...this.props,
-          ...this.sharedHandlers,
-          user: this.state.user,
-          error: this.state.error,
-        };
-
-        return <WrappedComponent {...props} />;
+        return error;
       }
     }
+
+    tryToSignInWithProvider = (
+      provider: PossibleProviders
+    ): Promise<firebase.auth.UserCredential> =>
+      this.tryTo<firebase.auth.UserCredential>(() => {
+        const providerInstance = providers[provider];
+
+        if (!providerInstance) {
+          throw new Error(getErrorMessageForProvider(provider));
+        }
+
+        return firebaseAppAuth.signInWithPopup(providerInstance);
+      });
+
+    signOut = () => this.tryTo<void>(() => firebaseAppAuth.signOut());
+
+    signInAnonymously = () =>
+      this.tryTo<firebase.auth.UserCredential>(() =>
+        firebaseAppAuth.signInAnonymously()
+      );
+
+    signInWithGithub = () => this.tryToSignInWithProvider("githubProvider");
+
+    signInWithTwitter = () => this.tryToSignInWithProvider("twitterProvider");
+
+    signInWithGoogle = () => this.tryToSignInWithProvider("googleProvider");
+
+    signInWithFacebook = () => this.tryToSignInWithProvider("facebookProvider");
+
+    signInWithEmailAndPassword = (email: string, password: string) =>
+      this.tryTo<firebase.auth.UserCredential>(() =>
+        firebaseAppAuth.signInWithEmailAndPassword(email, password)
+      );
+
+    signInWithPhoneNumber = (
+      phoneNumber: string,
+      applicationVerifier: firebase.auth.ApplicationVerifier
+    ) =>
+      this.tryTo<firebase.auth.ConfirmationResult>(() =>
+        firebaseAppAuth.signInWithPhoneNumber(phoneNumber, applicationVerifier)
+      );
+
+    createUserWithEmailAndPassword = (email: string, password: string) =>
+      this.tryTo<firebase.auth.UserCredential>(() =>
+        firebaseAppAuth.createUserWithEmailAndPassword(email, password)
+      );
+
+    sharedHandlers = {
+      signInWithEmailAndPassword: this.signInWithEmailAndPassword,
+      createUserWithEmailAndPassword: this.createUserWithEmailAndPassword,
+      signInWithGithub: this.signInWithGithub,
+      signInWithTwitter: this.signInWithTwitter,
+      signInWithGoogle: this.signInWithGoogle,
+      signInWithFacebook: this.signInWithFacebook,
+      signInWithPhoneNumber: this.signInWithPhoneNumber,
+      setError: this.setError,
+      signInAnonymously: this.signInAnonymously,
+      signOut: this.signOut
+    };
+
+    render() {
+      const props = {
+        ...this.props,
+        ...this.sharedHandlers,
+        loading: this.state.loading,
+        user: this.state.user,
+        error: this.state.error
+      };
+
+      return <WrappedComponent {...props} />;
+    }
+  };
 
 export default withFirebaseAuth;
